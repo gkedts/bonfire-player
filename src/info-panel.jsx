9843672bd42fb68d3c0047d646078be437fb70dc
gkedts <gkedts@gmail.com>
2015-05-28 01:49:45 -0400

    changed names of properties being passed in

--- src/info-panel.jsx
+++ src/info-panel.jsx
@@ -2,17 +2,20 @@ var React = require('react');
 var _ = require('underscore');
 
 var Tracklist = require('./tracklist.jsx');
+var FileBrowser = require('./file-browser.jsx');
 
 var InfoPanel = React.createClass({
   propTypes: {
-    current: React.PropTypes.object,
     tracklist: React.PropTypes.arrayOf(
       React.PropTypes.object),
     state: React.PropTypes.string,
     mopidy: React.PropTypes.object.isRequired,
+    local: React.PropTypes.arrayOf(
+      React.PropTypes.object),
   },
   render: function() {
     return <div className="info-panel">
+      <FileBrowser local={this.props.local} mopidy={this.props.mopidy}/>
       <Tracklist tracklist={this.props.tracklist} mopidy={this.props.mopidy}/>
     </div>
   },
